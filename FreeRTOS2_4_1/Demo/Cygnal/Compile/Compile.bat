9ddd6769f6d1692a8e056da6ca12b355e514f93e
Stefan Morley <stefan@djdts.com.au>
2023-07-30 13:23:26 +1000

    Added Notepad++ project. Corrected Compile/Compile.bat

--- FreeRTOS2_4_1/Demo/Cygnal/Compile/Compile.bat
+++ FreeRTOS2_4_1/Demo/Cygnal/Compile/Compile.bat
@@ -1,4 +1,4 @@
-REM only set required paths to prevent other make references
+REM only set required paths to prevent other "make" references
 REM taking precedence.
 set path=C:\Compilers\sdcc_2_5\bin;C:\cygwin64\bin
 make clean
